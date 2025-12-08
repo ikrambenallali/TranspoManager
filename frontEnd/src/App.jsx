b5e19025917893b3b6ce59a82b7191b9dc7a45f5
ikrambenallali <ikrambenallali2003@gmail.com>
2025-12-08 14:09:39 +0100

    installer projet react

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello React + Vite + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
