4f792ff000071203a5165820cc2848ee90d0be35
ikrambenallali <ikrambenallali2003@gmail.com>
2025-12-09 10:10:45 +0100

    logout

--- backEnd/Routes/authRoutes.js
+++ backEnd/Routes/authRoutes.js
@@ -3,5 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/login', AuthController.login);
+router.post('/logout', AuthController.logout);
 
 export default router;
