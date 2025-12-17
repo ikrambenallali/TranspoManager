97807c0a0de323b15e6b51232ab0eb0ad4ff8add
ikrambenallali <ikrambenallali2003@gmail.com>
2025-12-17 10:25:22 +0100

    TRAN-24 docker

--- backEnd/server.js
+++ backEnd/server.js
@@ -26,7 +26,10 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
     credentials: true
 }));
 app.use(express.json());
